1438ad37821870f4fc80e0907dd956d051e2d39c
sheepiiHD <sheepiiHD@gmail.com>
2016-05-16 14:29:01 -0400

    updated

---
+++ Marketer/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Marketer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.error_label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.information_label = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // error_label
+            // 
+            this.error_label.AutoSize = true;
+            this.error_label.Location = new System.Drawing.Point(61, 13);
+            this.error_label.Name = "error_label";
+            this.error_label.Size = new System.Drawing.Size(0, 13);
+            this.error_label.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(99, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Try again";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // information_label
+            // 
+            this.information_label.AutoSize = true;
+            this.information_label.Location = new System.Drawing.Point(96, 185);
+            this.information_label.Name = "information_label";
+            this.information_label.Size = new System.Drawing.Size(86, 13);
+            this.information_label.TabIndex = 2;
+            this.information_label.Text = "information_label";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(13, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(259, 82);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start Marketting";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Marketer.Properties.Resources.exit_button;
+            this.pictureBox1.Location = new System.Drawing.Point(233, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(39, 38);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 201);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(259, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.information_label);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.error_label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label error_label;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label information_label;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Marketer/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Reflection;
+
+namespace Marketer
+{
+    public partial class Form1 : Form
+    {
+        public string ProcessName = "Tibia";
+        public bool IsReady = false;
+
+        #region DLL Imports
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(UInt32 dwDesiredAccess, bool bInheritHandle, UInt32 dwProcessId);
+        [DllImport("kernel32.dll")]
+        public static extern Int32 CloseHandle(IntPtr hObject);
+        [DllImport("kernel32.dll")]
+        public static extern Int32 ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In, Out] byte[] buffer, UInt32 size, out IntPtr lpNumberOfBytesRead);
+        [DllImport("kernel32.dll")]
+        public static extern Int32 WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In, Out] byte[] buffer, UInt32 size, out IntPtr lpNumberOfBytesWritten);
+        [DllImport("user32.dll")]
+        public static extern int SetActiveWindow(int hwnd);
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(IntPtr hwnd);
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        static extern bool GetKeyboardState(byte[] lpKeyState);
+        [DllImport("user32.dll")]
+        static extern bool SetKeyboardState(byte[] lpKeyState);
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        
+
+        #endregion
+
+        #region Declares
+        IntPtr hProc = IntPtr.Zero;
+        private const int SW_SHOWMAXIMIZED = 3;
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        const uint dwAllAccess = 0x1F0FFF;
+        // byte[] bBuff = new byte[10];
+        // bool bFound = false;
+        bool running = false;
+        Thread marketThread;
+        Process p;
+        #endregion
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            button1.Visible = false;
+            button2.Visible = false;
+            information_label.Visible = false;
+            progressBar1.Visible = false;
+            inject();
+            information_label.AutoSize = false;
+            information_label.TextAlign = ContentAlignment.MiddleCenter;
+            information_label.Dock = DockStyle.None;
+            information_label.Left = 10;
+            information_label.Width = this.Width - 10;
+
+        }
+
+        private void inject()
+        {
+            if (Process.GetProcessesByName(ProcessName).Length == 1)
+            {
+                hProc = OpenProcess(dwAllAccess, true, (uint)Process.GetProcessesByName(ProcessName)[0].Id);
+                IsReady = true;
+                this.Text = "Marketer (Running)";
+                button1.Visible = false;
+                error_label.Text = "";
+                button2.Visible = true;
+                progressBar1.Visible = true;
+
+                //information label
+                information_label.Text = "This will start over the process.";
+                information_label.Visible = true;
+
+            }
+            else
+            {
+                this.Text = "Marketer (Stopped)";
+                error_label.Text = "Cannot inject. Process not opened.";
+
+                if (IsReady == false)
+                {
+                    button1.Visible = true;
+                    return;
+                }
+            }
+        }
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+        public static void RightMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_RIGHTUP, xpos, ypos, 0, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inject();
+        }
+        private void forceActivity()
+        {
+            if (running)
+            {
+
+            }
+            else
+            {
+
+
+            }
+        }
+        private void run()
+        {
+            if (connectionCheck())
+            {
+                if (running)
+                {
+                    this.BackColor = Color.LightGray;
+                    running = false;
+                    button2.Text = "Start Marketting.";
+                    turnOff();
+                }
+                else
+                {
+                    information_label.Text = "Force Stop Marketting.";
+                    button2.Text = "Force Stop.";
+                    this.BackColor = Color.Red;
+                    running = true;
+                    turnOn();
+                }
+            }
+        }
+       
+
+
+        private void turnOn()
+        {
+            int i = 0;
+
+            string[] worlds = { "Antica", "Next World" };
+            BringWindowToFront();
+            Thread.Sleep(1000);
+            LeftMouseClick(2008, 840);
+            Thread.Sleep(1000);
+            information_label.Text = "Logging in.";
+            SendKeys.SendWait("marketaccount");
+            Thread.Sleep(500);
+            SendKeys.SendWait("{TAB}");
+            SendKeys.SendWait(""); //Password here
+            Thread.Sleep(500);
+            SendKeys.SendWait("{ENTER}");
+            Thread.Sleep(2000);
+            SendKeys.SendWait("{ENTER}");
+            information_label.Text = "Logged In! Currently on world: " + worlds[i];
+            Thread thread = new Thread(doMarket);
+            thread.Start();
+
+        }
+        private void doMarket()
+        {
+            while (running){
+                Console.WriteLine("x: " + Cursor.Position.X + " y: " + Cursor.Position.Y);
+            }
+        }
+        private void turnOff()
+        {
+            information_label.Text = "Logging off.";
+            BringWindowToFront();
+            Thread.Sleep(500);
+            LeftMouseClick(3823, 356);
+            Thread.Sleep(500);
+            SendKeys.SendWait("{ENTER}");
+            Thread.Sleep(500);
+            SendKeys.SendWait("{ESC}");
+
+
+
+            information_label.Text = "Logged off, you're safe to restart it again. =)";
+        }
+        private void rotate()
+        {
+            SendKeys.Send("^(Q)");
+            SendKeys.Send("{DOWN}");
+            SendKeys.Send("{~}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            run();
+        }
+        private bool connectionCheck()
+        {
+            if (Process.GetProcessesByName(ProcessName).Length == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void BringWindowToFront()
+        {
+            Process[] arrProcesses = Process.GetProcessesByName("Tibia");
+            if (arrProcesses.Length > 0)
+            {
+                ShowWindow(arrProcesses[0].MainWindowHandle, SW_SHOWMAXIMIZED);
+                IntPtr ipHwnd = arrProcesses[0].MainWindowHandle;
+                Thread.Sleep(100);
+                SetForegroundWindow(ipHwnd);
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        public Array worldnames()
+        {
+
+            string[] worlds = {"Antica", "Next World"};
+            return worlds.ToArray();
+        }
+    }
+}
